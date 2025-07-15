c15403f4872bd9296eddfec351a2b4fec91905f3
barsheel <barsheel@gmail.com>
2025-07-15 12:59:52 +0700

    install twig and create index page
    - install twig and dependecies in composer
    - create index.twig
    - add handler

--- public/index.php
+++ public/index.php
@@ -1,16 +1,37 @@
 <?php
 
-$autoloadGlobalPath =__DIR__ . '/../../../autoload.php';
-$autoloadLocalPath =__DIR__ . '/../vendor/autoload.php';
+$autoloadGlobalPath = __DIR__ . '/../../../autoload.php';
+$autoloadLocalPath = __DIR__ . '/../vendor/autoload.php';
 file_exists($autoloadLocalPath) ? require_once $autoloadLocalPath : require_once $autoloadGlobalPath;
 
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
-$app = AppFactory::create();
+$container = new Container();
+
+$container->set(Twig::class, function () {
+    return Twig::create(__DIR__ . '/../templates', [
+        'cache' => 'cache', // Полностью отключаем кеш
+        'auto_reload' => true, // Автоматически проверяем изменения
+        'debug' => true // либо Включаем режим отладки
+    ]);
+});
+
+$app = AppFactory::createFromContainer($container);
+
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
 $app->addErrorMiddleware(true, true, true);
 
+
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
+
+    $params = ['test' => 'test'];
+
+    $twig = $this->get(Twig::class);
+
+    return $twig->render($response, 'index.twig', $params);
 });
 
 $app->run();
